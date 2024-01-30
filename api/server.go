a911a4b7cc5494c7b4a866f56e8f99a7baf3a855
nobelium24 <ogunbaja07@gmail.com>
2024-01-30 10:05:41 +0100

    started workinh on api with gin

---
+++ api/server.go
@@ -0,0 +1,32 @@
+package api
+
+import (
+	"github.com/gin-gonic/gin"
+	db "github.com/techschool/simplebank/db/sqlc"
+)
+
+type Server struct {
+	store  *db.Store
+	router *gin.Engine
+}
+
+func NewServer(store *db.Store) *Server {
+	server := &Server{store: store}
+	router := gin.Default()
+
+	router.POST("/accounts", server.createAccount)
+	router.GET("/accounts/:ID", server.getAccount)
+	router.GET("/accounts", server.listAccount)
+
+
+	server.router = router
+	return server
+}
+
+func (server* Server) Start (address string) error {
+	return server.router.Run(address)
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
+}
